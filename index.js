d0902d8961eb3228596236f5f71bc98b85bc940f
jeonghwan <ej88ej@gmail.com>
2014-11-13 15:04:33 +0900

    Modulize routes

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ var Hapi = require('hapi');
 
 var server = new Hapi.Server('localhost', 8000);
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function(req, reply) {
-    reply('server is running');
-  }
-});
+var routes = require('./routes');
+
+routes(server);
 
 server.start();
--- routes.js
+++ routes.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var Joi = require('joi');
-
-var func1 = require('./api/func1.js'),
-  func2 = require('./api/func2.js');
-
 module.exports = function(server) {
 
-    server.route({ method: 'GET', path: '/', handler: func1 });
-
-    server.route({ method: 'GET', path: '/{name}', handler: func2, config: {
-        validate: {
-            params: {
-                name: Joi.string().min(3).max(10)
-            }
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(req, reply) {
+            reply('server is running');
         }
-    }});
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/hello/{name}',
+        handler: function(req, reply) {
+            var name = req.params.name;
+            reply('Hello ' + name);
+        }
+    });
 
 };
